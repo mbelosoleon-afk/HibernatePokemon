42eac787e5be524ce1803bab7ad76f022966f8d2
dam <mbelosoleon@danielcastelao.org>
2025-11-10 09:05:43 +0100

    clase Pokemon

---
+++ src/Model/Pokemon.java
@@ -0,0 +1,67 @@
+package Model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "pokemon")
+public class Pokemon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "nome")
+    String nome;
+
+    @Column(name = "nacemento")
+    Date nacemento;
+
+    @OneToOne
+    @JoinColumn(name = "pokedexentry")
+    Pokedex pokedexentry;
+
+    @ManyToOne
+    @JoinColumn(name = "adestrador")
+    Adestrador adestrador;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Adestrador getAdestrador() {
+        return adestrador;
+    }
+
+    public void setAdestrador(Adestrador adestrador) {
+        this.adestrador = adestrador;
+    }
+
+    public Pokedex getPokedexentry() {
+        return pokedexentry;
+    }
+
+    public void setPokedexentry(Pokedex pokedexentry) {
+        this.pokedexentry = pokedexentry;
+    }
+
+    public Date getNacemento() {
+        return nacemento;
+    }
+
+    public void setNacemento(Date nacemento) {
+        this.nacemento = nacemento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
